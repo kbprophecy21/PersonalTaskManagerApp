de50e1c75f628c7c1d5c19bccc666f77ff3dce7f
Kyle <82291919+kbprophecy21@users.noreply.github.com>
2025-04-08 05:17:28 +0000

    got the main file going. need to plan better and get some ideas flowing more. so i can practice better.

---
+++ main.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace taskmanagerproject {
+
+    class MainProgram {
+
+        List<string> tasks = [];
+
+        public List<string> showAllTask(){
+            return tasks;
+        }
+
+
+        static void Main(String[] args) {
+
+            Console.writeline("Hello im working properly");
+
+
+        }   
+
+    }
+
+}
+
